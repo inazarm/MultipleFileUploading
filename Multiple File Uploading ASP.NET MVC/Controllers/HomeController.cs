f7a49c33e44e710e37d6deb9d2223f3dd4b54f4f
inazarm <inazar.m@gmail.com>
2022-05-29 15:58:13 +0500

    uploading mutilple file from multiple buttons on single page and submiting once all

--- Multiple File Uploading ASP.NET MVC/Controllers/HomeController.cs
+++ Multiple File Uploading ASP.NET MVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,5 +27,27 @@ namespace Multiple_File_Uploading_ASP.NET_MVC.Controllers
 
             return View();
         }
+        public ActionResult Uploading()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Uploading(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return View();
+
+        }
+
     }
 }
